031b6b22f903f2bfa238cdb2d514b00daf069edf
Kyle Lynch <kylelynch104@gmail.com>
2019-05-26 16:14:44 -0800

    version_0.1.0

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = new Set(getState().posts.map(({ userId }) => userId));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const res = await jsonPlaceholder.get('/posts');
 
@@ -11,3 +17,4 @@ export const fetchUser = id => async dispatch => {
 
   dispatch({ type: 'FETCH_USER', payload: res.data });
 };
+
--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserName extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -27,6 +22,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(
-  mapStateToProps,
-  { fetchUser }
+  mapStateToProps
 )(UserName);
